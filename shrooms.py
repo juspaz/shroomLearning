631a278a3a81057288440accdb6dbae9dc860b32
Justas Pazereckas <justas.pazereckas@ktu.edu>
2019-05-19 22:11:50 +0300

    Done experiments and testing

--- shrooms.py
+++ shrooms.py
@@ -3,42 +3,18 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import fbeta_score, accuracy_score
 from sklearn.model_selection import KFold as kfold
-# from time import time
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
 
-def train_predict(learner, sample_size, X_train, y_train, X_test, y_test):
-    '''
-    inputs:
-       - learner: the learning algorithm to be trained and predicted on
-       - sample_size: the size of samples (number) to be drawn from training set
-       - X_train: features training set
-       - y_train: income training set
-       - X_test: features testing set
-       - y_test: income testing set
-    '''
-
-    results = {}
-
-    learner = learner.fit(X_train[:sample_size], y_train[:sample_size])
-    predictions_test = learner.predict(X_test)
-    predictions_train = learner.predict(X_train[:300])
-
-    results['acc_train'] = accuracy_score(y_train[:300], predictions_train)
-    results['acc_test'] = accuracy_score(y_test, predictions_test)
-    results['f_train'] = fbeta_score(y_train[:300], predictions_train, beta=0.5)
-    results['f_test'] = fbeta_score(y_test, predictions_test, beta=0.5)
-
-    print("{} trained on {} samples.".format(learner.__class__.__name__, sample_size))
-
-    return results
 
 def train_predict2(learner, X_train, y_train, X_test, y_test):
     '''
     inputs:
        - learner: the learning algorithm to be trained and predicted on
-       - sample_size: the size of samples (number) to be drawn from training set
        - X_train: features training set
        - y_train: income training set
        - X_test: features testing set
@@ -50,26 +26,30 @@ def train_predict2(learner, X_train, y_train, X_test, y_test):
     learner = learner.fit(X_train, y_train)
 
     predictions_test = learner.predict(X_test)
-    
-    predictions_train = learner.predict(X_train)
-    results['acc_train'] = accuracy_score(y_train, predictions_train)
+    predictions_train = learner.predict(X_train[:300])
+
+    results['acc_train'] = accuracy_score(y_train[:300], predictions_train)
     results['acc_test'] = accuracy_score(y_test, predictions_test)
-    results['f_train'] = fbeta_score(y_train, predictions_train, beta=0.5)
-    results['f_test'] = fbeta_score(y_test, predictions_test, beta=0.5)
+    results['f_train'] = fbeta_score(y_train[:300], predictions_train, beta=0.1)
+    results['f_test'] = fbeta_score(y_test, predictions_test, beta=0.1)
 
     print("{} trained on {} samples.".format(learner.__class__.__name__, len(X_train)))
 
     return results
+
+
 data = pd.read_csv("agaricus-lepiota.csv")
 
-print(len(data))
+# x = data['class']
+# ax = sns.countplot(x=x, data=data)
+
+# data = data.drop(["gill-color"],axis=1)
 
 for index, row in data.iterrows():
     for name, values in data.iteritems():
         if row[name] == '?':
             data = data.drop(index)
 
-print(len(data))
 
 target = 'class' # The class we want to predict
 labels = data[target]
@@ -84,30 +64,23 @@ features.drop(categorical, axis=1, inplace=True) # Drop the original feature, le
 
 labels = pd.get_dummies(labels)['p'] # Encode the target class, 1 is deadly 0 is safe
 
-#X_train, X_test, y_train, y_test = train_test_split(features,labels, test_size=0.2, random_state=0)
-
 clf_A = GaussianNB()
-clf_C = RandomForestClassifier(n_estimators=3)
-clf_B = KNeighborsClassifier()
+clf_C = RandomForestClassifier(n_estimators=100, oob_score=True, max_features=5)
+clf_B = KNeighborsClassifier(n_neighbors=100)
 
-kf = kfold(n_splits=10,shuffle=True)
+kf = kfold(n_splits=5,shuffle=True)
 xset = np.array(features)
 yset= np.array(labels)
-print ("Atskirta")
+
 results = {}
 for clf in [clf_A, clf_B, clf_C]:
     clf_name = clf.__class__.__name__
     results[clf_name] = {}
     i=0
     for train_ind, test_ind in kf.split(features, labels):
-        
         X_train, X_test = xset[train_ind], xset[test_ind]
         y_train, y_test = yset[train_ind], yset[test_ind]
-        
-        #print("Train")
-        #print(len(X_train))
-        #print("Test")
-        #print(len(X_test))
+
         results[clf_name][i] = train_predict2(clf,X_train, y_train, X_test, y_test)
         print(results[clf_name][i])
         i=i+1
@@ -127,3 +100,5 @@ for clf in [clf_A, clf_B, clf_C]:
 #        results[clf_name][i] = \
 #        train_predict(clf, samples, X_train, y_train, X_test, y_test)
 #        print(results[clf_name][i])
+
+plt.show()
